e8e25e38df768cc831312e711d2d1594c3f4dcda
Cloud-Propel <propelcloud@gmail.com>
2025-01-30 18:35:28 +0530

    Rent Video

--- src/main/java/com/rentvideo/RentVideoApplication.java
+++ src/main/java/com/rentvideo/RentVideoApplication.java
@@ -1,5 +1,13 @@
 package com.rentvideo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class RentVideoApplication {
-    
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentVideoApplication.class, args);
+        System.out.println("🚀 RentVideo API is running...");
+    }
 }
